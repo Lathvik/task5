9f300afcfa618c817524558cf35aa8ab139ca959
lathvik <lathvik.2021@vitstudent.ac.in.com>
2023-10-14 23:27:06 +0530

    first commit

---
+++ task5/src/App.js
@@ -0,0 +1,12 @@
+
+// src/App.js
+import React from 'react';
+import AgeCalculator from './AgeCalculator';
+ 
+function App() {
+  return (
+    <div className="App">
+      <AgeCalculator />
+    </div>
+  );
+}
